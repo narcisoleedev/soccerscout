ba06df1651ded21d4426a40102559eee434df89a
Guilherme Lee <narcisolee2002@usp.br>
2023-08-30 12:23:56 -0300

    requirements.txt + competitions.json treatment

--- src/matchsep.py
+++ src/matchsep.py
@@ -1,6 +1,7 @@
 import os
 import json
 import numpy as np
+import pandas as pd
 
 #Paths
 path = os.path.abspath('../') + '/open-data/data/'
@@ -16,4 +17,14 @@ for id in os.listdir(eventsPath):
 matchIds = np.unique(matchIds)
 print(matchIds)
 
+def competitions_org():
+    competitionsPath = path + 'competitions.json'
+    competitionsFile = open(competitionsPath, 'r')
+    competitionsDict = json.load(competitionsFile)
+    #print(competitionsDict)
+    competitions = pd.DataFrame(competitionsDict)
+    keysComp = ['competition_id', 'season_id', 'competition_name', 'competition_gender','country_name', 'season_name']
+    competitions = competitions[keysComp]
+    print(competitions)
 
+competitions_org()
