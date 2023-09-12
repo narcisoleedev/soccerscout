89909e14b4b23289ceaea6b034391649f1059604
Guilherme Lee <narcisolee2002@usp.br>
2023-09-12 11:48:40 -0300

    trying to org matches in their current directory but there is a mistake during df.to_csv

--- src/matchsep.py
+++ src/matchsep.py
@@ -18,7 +18,7 @@ for id in os.listdir(eventsPath):
     matchIds.append(id.partition('.')[0])
 
 matchIds = np.unique(matchIds)
-print(matchIds)
+print(len(matchIds))
 
 def competitions_json():
     competitionsPath = path + 'competitions.json'
@@ -27,7 +27,7 @@ def competitions_json():
     competitions = pd.DataFrame(competitionsDict)
     keysComp = ['competition_id', 'season_id', 'competition_name', 'competition_gender','country_name', 'season_name']
     competitions = competitions[keysComp]
-    print(competitions)
+    #print(competitions)
     competitions_org(competitions)
     
 def competitions_org(competitions):
@@ -36,26 +36,36 @@ def competitions_org(competitions):
         seasonNoSlice = s.replace('/', '-')
         os.system('mkdir ' + pathOrg + seasonNoSlice)
         genderPerSeason = np.unique(competitions.loc[competitions['season_name']==s, 'competition_gender'])
+        #Genero per season
         for g in genderPerSeason:
             os.system('mkdir ' + pathOrg + seasonNoSlice + '/"' + g + '"')
             genderFilter = competitions[competitions['competition_gender']==g]
             countryPerSeason = np.unique(genderFilter.loc[genderFilter['season_name']==s, 'country_name'])
+            #Organizar os arquivos por pais
             for ct in countryPerSeason:
                 os.system('mkdir ' + pathOrg + seasonNoSlice + '/"' + g + '"' + '/"' + ct + '"')
                 countryFilter = genderFilter[genderFilter['country_name']==ct]
-                print(countryFilter)
+                #print(countryFilter)
                 competitionsPerSeason = np.unique(countryFilter.loc[countryFilter['season_name']==s, 'competition_name'])
+                #Organizar os arquivos por competicoes
                 for c in competitionsPerSeason:
                     os.system('mkdir ' + pathOrg + seasonNoSlice + '/"' + g + '"' + '/"' + ct + '"' + '/"' + c + '"')
 
-def matches_org():
+def matches_json():
     matchesDict = []
-    for f in os.listdir(matchesPath):
-        matchesFile = open(matchesPath + f, 'r')
-        matchesDict = json.load(matchesFile)
-        print(matchesDict)
-    matchesDict = json.load(matchesFile)
-    
-
-competitions_json()
-matches_org()
+    i = 0
+    for c in os.listdir(matchesPath):
+        for f in os.listdir(matchesPath + '/' + c):
+            matchesFile = open(matchesPath + c + '/' + f, 'r')
+            matchesDict = pd.json_normalize(json.load(matchesFile))
+            matchesDict = pd.DataFrame(matchesDict)
+            try:
+                path = pathOrg+str(matchesDict['season.season_name']).replace('/', '-')+'/'+(matchesDict['home_team.home_team_gender'])+'/"'+matchesDict['competition.country_name']+'"/"'+ matchesDict['competition.competition_name']+'"/'+matchesDict['home_team.home_team_name']+'-'+matchesDict['away_team.away_team_name']+'('+matchesDict['match_date']+')'+'.json'
+                print(path)
+                #matchesDict.to_csv(path, sep='|')
+            except:
+                i = i + 1
+            #print((matchesDict['season.season_name']).astype(str)).replace('/', '-')
+    print(i)
+#competitions_json()
+matches_json()
