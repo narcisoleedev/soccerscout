401fa83f75e7d635956cdf8045ef1028661906d6
Guilherme Lee <narcisolee2002@usp.br>
2023-09-20 09:08:08 -0300

    create data folder and eventos.py added

---
+++ data/src/eventos.py
@@ -0,0 +1,348 @@
+import pandas as pd
+
+def _parse_pass(row):
+    
+    string_base = "pass"  # default
+    string_type = string_base + ".type" + ".name"
+    string_height = string_base + ".height"+ ".name"
+    string_cross = string_base + ".cross"
+    if row[string_type] == 'Free Kick':
+        if row[string_height] == 'High Pass' or row[string_cross]:
+            action = 'freekick_crossed'
+        else:
+            action = 'freekick_short'
+    elif row[string_type] == 'Corner':
+        if row[string_height] == 'High Pass' or row[string_cross]:
+            action = 'corner_crossed'
+        else:
+            action = 'corner_short'
+    elif row[string_type] == 'Goal Kick':
+        action = 'goalkick'
+    elif row[string_type] == 'Throw-in':
+        action = 'throw_in'
+    elif row[string_cross] == True:
+        action = 'cross'
+    else:
+        action = 'pass'
+
+    string_outcome = "pass"+".outcome"+".name"
+    if row[string_outcome] in ['Incomplete', 'Out']:
+        result = 'fail'
+    elif row[string_outcome] == 'Pass Offside':
+        result = 'offside'
+    elif row[string_outcome] in ['Injury Clearance', 'Unknown']:
+        
+        action = 'non_action'
+        result = 'success'
+    else:
+        result = 'success'
+
+    string_body_part = "pass" + ".body_part"+".name"
+    if row[string_body_part] is None:
+        body_part = 'foot'
+    elif 'Head' in row[string_body_part]:
+        body_part = 'head'
+    elif row[string_body_part] == 'Left Foot':
+        body_part = 'foot_left'
+    elif row[string_body_part] == 'Right Foot':
+        body_part = 'foot_right'
+    elif 'Foot' in row[string_body_part] or row[string_body_part] == 'Drop Kick':
+        body_part = 'foot'
+    else:
+        body_part = 'other'
+
+    return action, result, body_part
+
+def _parse_dribble(row):
+    
+    action = 'take_on'
+    string = "dribble" + ".outcome"+".name"
+    if row[string] == 'Incomplete':
+        result = 'fail'
+    else:
+        result = 'success'
+
+    body_part = 'foot'
+
+    return action, result, body_part
+
+def _parse_carry():
+    
+    action = 'dribble'
+    result = 'success'
+    body_part = 'foot'
+
+    return action, result, body_part
+
+def _parse_foul_committed(row):
+
+    action = 'foul'
+    
+    string = "foul_committed.card.name"
+    try:
+        if 'Yellow' in row[string]:
+            result = 'yellow_card'
+        else:
+            result = 'red_card'
+
+    except:
+            result = 'success'
+
+    body_part = 'foot'
+
+    return action, result, body_part
+    
+def _parse_duel(row):
+    string = "duel"
+    if row[string+".type.name"] == 'Tackle':
+        action = 'tackle'
+        duel_outcome = row[string+".outcome"+".name"]
+        if duel_outcome in ['Lost In Play', 'Lost Out']:
+            result = 'fail'
+        elif duel_outcome in ['Success in Play', 'Won']:
+            result = 'success'
+        else:
+            result = 'success'
+
+        body_part = 'foot'
+
+    else:
+        action = 'non_action'
+        result = 'success'
+        body_part = 'foot'
+
+    return action, result, body_part
+
+def _parse_interception(row):
+    action = 'interception'
+    string = "interception.outcome.name"
+    if row[string] in ['Lost In Play', 'Lost Out']:
+        result = 'fail'
+    elif row[string] == 'Won':
+        result = 'success'
+    else:
+        result = 'success'
+    
+    body_part = 'foot'
+    
+    return action, result, body_part
+
+def _parse_shot(row):
+    string_base = "shot"
+
+    string_type = string_base+".type"+".name"
+
+    if row[string_type] == 'Free Kick':
+        action = 'shot_freekick'
+    elif row[string_type] == 'Penalty':
+        action = 'shot_penalty'
+    else:
+        action = 'shot'
+
+    string_outcome = string_base+'.outcome'+'.name'
+    if row[string_outcome] == 'Goal':
+        result = 'success'
+    elif row[string_outcome] in ['Blocked', 'Off T', 'Post', 'Saved', 'Wayward']:
+        result = 'fail'
+    else:
+        result = 'fail'
+
+    string_body_part = string_base+".body_part"+".name"
+    
+    if row[string_body_part] is None:
+        body_part = 'foot'
+    elif 'Head' in row[string_body_part]:
+        body_part = 'head'
+    elif row[string_body_part] == 'Left Foot':
+        body_part = 'foot_left'
+    elif row[string_body_part] == 'Right Foot':
+        body_part = 'foot_right'
+    elif 'Foot' in row[string_body_part]:
+        body_part = 'foot'
+    else:
+        body_part = 'other'
+
+    return action, result, body_part
+    
+def _parse_owngoal():
+
+    action = 'bad_touch'
+    result = 'owngoal'
+    body_part = 'foot'
+
+    return action, result, body_part
+
+def _parse_goalkeeper(row):
+
+    string = 'goalkeeper' + '.type' + '.name'
+    if row[string] == 'Shot Saved':
+        action = 'keeper_save'
+    elif row[string] in ('Collected', 'Keeper Sweeper'):
+        action = 'keeper_claim'
+    elif row[string] == 'Punch':
+        action = 'keeper_punch'
+    else:
+        action = 'non_action'
+
+    string_outcome = 'goalkeeper'+'.outcome'+'.name'
+    if row[string_outcome] in [
+        'Claim',
+        'Clear',
+        'Collected Twice',
+        'In Play Safe',
+        'Success',
+        'Touched Out',
+    ]:
+        result = 'success'
+    elif row[string_outcome] in ['In Play Danger', 'No Touch']:
+        result = 'fail'
+    else:
+        result = 'success'
+
+    string_body_part = 'goalkeeper'+'.body_part'+'.name'
+    if row[string_body_part] is None:
+        body_part = 'foot'
+    elif 'Head' in row[string_body_part]:
+        body_part = 'head'
+    elif row[string_body_part] == 'Left Foot':
+        body_part = 'foot_left'
+    elif row[string_body_part] == 'Right Foot':
+        body_part = 'foot_right'
+    elif 'Foot' in row[string_body_part] or row[string_body_part] == 'Drop Kick':
+        body_part = 'foot'
+    else:
+        body_part = 'other'
+
+    return action, result, body_part
+
+def _parse_clearance(row):
+    action = 'clearance'
+    result = 'success'
+    string_body_part = "clearance"+".body_part"+".name"
+    if row[string_body_part] is None:
+        body_part = 'foot'
+    elif 'Head' in row[string_body_part]:
+        body_part = 'head'
+    elif row[string_body_part] == 'Left Foot':
+        body_part = 'foot_left'
+    elif row[string_body_part] == 'Right Foot':
+        body_part = 'foot_right'
+    elif 'Foot' in row[string_body_part]:
+        body_part = 'foot'
+    else:
+        body_part = 'other'
+    
+    return action, result, body_part
+
+def _parse_miscontrol():
+    
+    action = 'bad_touch'
+    result = 'fail'
+    body_part = 'foot'
+    
+    return action, result, body_part
+
+def _find_loc(linha):
+    try:
+        return linha[str(linha["type.name"]).lower() +".end_location"]
+    except:
+        return None
+
+def _find_end_time(df,indice):
+    try: 
+    
+        return  df.iloc[indice + 1]["timestamp"]
+    
+    except:
+    
+        return None
+
+def _parse_events():
+    lista = ["Pass","Dribble","Carry","Foul Committed","Duel","Interception","Shot","Own Goal Against","Goal Keeper","Clearance","Miscontrol"]
+    
+    df = pd.read_csv('jogo.csv',  sep="|") 
+    df = df.where(pd.notnull(df), None)
+    
+    data = {
+    'StartTime': [],
+    'EndTime': [],
+    'StartLoc': [],
+    'EndLoc': [],
+    'Player': [],
+    'Team': [],
+    'ActionType': [],
+    'BodyPart': [],
+    'Result': []
+    }
+
+    df2 = pd.DataFrame(data)
+
+    for indice, linha in df.iterrows():
+        if linha["type.name"] in lista:
+            
+            if linha["type.name"] == "Pass":
+                action, result, body_part = _parse_pass(linha)
+            elif linha["type.name"] == "Dribble":
+                action, result, body_part = _parse_dribble(linha)
+            elif linha["type.name"] == "Interception":
+                action, result, body_part = _parse_interception(linha)
+            elif linha["type.name"] == "Shot":
+                action, result, body_part = _parse_shot(linha)
+            elif linha["type.name"] == "Goal Keeper":
+                action, result, body_part = _parse_goalkeeper(linha)
+            elif linha["type.name"] == "Clearance":
+                action, result, body_part = _parse_clearance(linha)
+            elif linha["type.name"] == "Foul Committed":                
+                action, result, body_part = _parse_foul_committed(linha)
+            elif linha["type.name"] == "Duel":
+                action, result, body_part = _parse_duel(linha)
+            elif linha["type.name"] == "Miscontrol":
+                action, result, body_part = _parse_miscontrol()
+            elif linha["type.name"] == "Carry":
+                action, result, body_part = _parse_carry()
+            elif linha["type.name"] == "Own Goal Against":
+                action, result, body_part = _parse_owngoal()
+
+
+            linha_temp = { 'StartTime': linha["timestamp"],
+                           'EndTime': _find_end_time(df,indice),
+                           'StartLoc': linha["location"],
+                           'EndLoc': _find_loc(linha),
+                           'Player': linha["player.id"],
+                           'Team': linha["team.id"],
+                           'ActionType': action,
+                           'BodyPart': body_part, 
+                           'Result': result
+                           }
+
+            df2 = df2.append(linha_temp, ignore_index=True)
+    
+    return df2
+
+if __name__ == "__main__":
+    
+    df = _parse_events()
+    df.to_csv("jogo_spadl.csv")
+
+#StartTime: the action’s start time,
+#EndTime: the action’s end time,
+#StartLoc: the (x,y) location where the action started,
+#EndLoc: the (x,y) location where the action ended,
+#Player: the player who performed the action,
+#Team: the player’s team,
+#ActionType: the type of the action (e.g., pass, shot, dribble),
+#BodyPart: the player’s body part used for the action,
+#Result: the result of the action (e.g., success or fail).
+        
+
+        #Pass
+        #Dribble
+        #Carry
+        #Foul Committed
+        #Duel
+        #Interception
+        #Shot
+        #Own Goal Against
+        #Goal Keeper
+        #Clearance
+        #Miscontrol
---
+++ data/src/matchsep.py
@@ -0,0 +1,75 @@
+import os
+import json
+import numpy as np
+import pandas as pd
+
+#Paths
+path = os.path.abspath('../') + '/open-data/data/'
+eventsPath = path + 'events/'
+lineupsPath = path + 'lineups/'
+matchesPath = path + 'matches/'
+
+pathOrg = os.path.abspath('../') + '/org-data/'
+
+#array of matches
+matchIds = []
+
+for id in os.listdir(eventsPath):
+    matchIds.append(id.partition('.')[0])
+
+matchIds = np.unique(matchIds)
+   
+def events_to_csv(x):
+    id = x['match_id']
+    eventsDF = pd.json_normalize(json.load(open(eventsPath + str(id) + '.json', 'r')))
+    sn = str(x['season.season_name']).replace('/', '-')
+    path = pathOrg+sn+'/'+x['home_team.home_team_gender']+'/"'+x['competition.country_name']+'"/"'+x['competition.competition_name']+'"/"'+x['home_team.home_team_name']+'-'+x['away_team.away_team_name']+'('+x['match_date']+')".csv'
+    path = pathOrg+sn+'/'+x['home_team.home_team_gender']+'/'+x['competition.country_name']+'/'+x['competition.competition_name']+'/'+x['home_team.home_team_name']+'-'+x['away_team.away_team_name']+'('+x['match_date']+').csv'
+    try:
+        eventsDF.to_csv(path, sep='|')
+    except:
+        print('There is no match with this id!')
+
+def competitions_json():
+    competitionsPath = path + 'competitions.json'
+    competitionsFile = open(competitionsPath, 'r')
+    competitionsDict = json.load(competitionsFile)
+    competitions = pd.DataFrame(competitionsDict)
+    keysComp = ['competition_id', 'season_id', 'competition_name', 'competition_gender','country_name', 'season_name']
+    competitions = competitions[keysComp]
+    #print(competitions)
+    competitions_org(competitions)
+    
+def competitions_org(competitions):
+    seasonNameUnique = np.unique(competitions['season_name'])
+    for s in seasonNameUnique:
+        seasonNoSlice = s.replace('/', '-')
+        os.system('mkdir ' + pathOrg + seasonNoSlice)
+        genderPerSeason = np.unique(competitions.loc[competitions['season_name']==s, 'competition_gender'])
+        #Genero per season
+        for g in genderPerSeason:
+            os.system('mkdir ' + pathOrg + seasonNoSlice + '/"' + g + '"')
+            genderFilter = competitions[competitions['competition_gender']==g]
+            countryPerSeason = np.unique(genderFilter.loc[genderFilter['season_name']==s, 'country_name'])
+            #Organizar os arquivos por pais
+            for ct in countryPerSeason:
+                os.system('mkdir ' + pathOrg + seasonNoSlice + '/"' + g + '"' + '/"' + ct + '"')
+                countryFilter = genderFilter[genderFilter['country_name']==ct]
+                #print(countryFilter)
+                competitionsPerSeason = np.unique(countryFilter.loc[countryFilter['season_name']==s, 'competition_name'])
+                #Organizar os arquivos por competicoes
+                for c in competitionsPerSeason:
+                    os.system('mkdir ' + pathOrg + seasonNoSlice + '/"' + g + '"' + '/"' + ct + '"' + '/"' + c + '"')
+
+def matches_json():
+    for c in os.listdir(matchesPath):
+        for f in os.listdir(matchesPath + '/' + c):
+            matchesFile = open(matchesPath + c + '/' + f, 'r')
+            matchesDict = pd.json_normalize(json.load(matchesFile))
+            matchesDF = pd.DataFrame(matchesDict)
+            matchesDF.apply(events_to_csv, axis=1)
+            #eventsDF = pd.json_normalize(json.load(open(eventsPath + e, 'r')))
+            #matchesDict.to_csv(path, sep='|')
+
+competitions_json()
+matches_json()
