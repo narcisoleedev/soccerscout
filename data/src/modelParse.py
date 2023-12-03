3c0cabb4dd732c9c806d9d869fe6725288fb5511
EdgarLiraa <edgar.jj.29@gmail.com>
2023-12-02 21:17:49 -0300

    edit para salvar em outra pasta

--- data/src/modelParse.py
+++ data/src/modelParse.py
@@ -1,28 +1,44 @@
 #Author: narcisoleedev
-
 import os
 import pandas as pd
 import add_features 
 import add_labels
 
 #Path to proc-data
+
 path = os.path.abspath('../')+'/proc-data'
 
 def listProcData(path):
     #List all itens in the proc-data dir
+    new_name_dir = path.replace("proc-data", "feature-data")
+    
+    if not os.path.exists(new_name_dir) and os.path.isdir(path):
+        os.mkdir(new_name_dir)   
+
     for item in os.listdir(path):
         #If the item on listdir is not a csv file it will recursively go to the next dir.
-        print(item)
+        #print(item)
         if os.path.isdir(path+'/'+item):
             subPath = path+'/'+item
             listProcData(subPath)
         #If it is it will insert the features and the labels
         else:
-            df = pd.read_csv(path+'/'+item, sep='|')
-            df = add_features.features(path+'/'+item)
-            df_labels = add_labels.labels(path+'/'+item)
-            df = pd.concat([df, df_labels[['Scores', 'Concedes']]], axis=1)
-            #df = pd.get_dummies(df, columns=df.columns)
-            df.to_csv(path+'/'+item, sep='|', index=False)
+            
+            path_temp = path
+            path_temp = path_temp.replace("proc-data", "feature-data")
+            
+            if os.path.exists(path_temp+"/"+item):
+            
+                print("Já existe")
+            
+            else:
+        
+                df = pd.read_csv(path+'/'+item, sep='|')
+                df = add_features.features(path+'/'+item)
+                df_labels = add_labels.labels(path+'/'+item)
+                df = pd.concat([df, df_labels[['Scores', 'Concedes']]], axis=1)
+                #df = pd.get_dummies(df, columns=df.columns)
+
+                df.to_csv(path_temp+'/'+item, sep='|', index=False)
 
 listProcData(path)
