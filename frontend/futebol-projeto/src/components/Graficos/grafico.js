83585c1a3229b55e92085aecced52d0d09271b64
GuilhermeC <guilhermecavalanti@gmail.com>
2023-12-05 16:58:23 -0300

    arrumei o grafico

--- frontend/futebol-projeto/src/components/Graficos/grafico.js
+++ frontend/futebol-projeto/src/components/Graficos/grafico.js
@@ -1,21 +1,9 @@
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LabelList, Legend, Label } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend, Label } from 'recharts';
 import './grafico.css'
 
-
 function Grafico({data}){
-    const label_y = "Número médio \nde ações em \n90 minutos"
-    const CustomLabel = (active, texto) => {
-        if(active){
-        return(
-            <div classname="Label_eixo">
-            <p>{texto}</p>
-            </div>
-        )
-    }
-    }
     const CustomTooltip = ({ active, payload, label }) => {
         if (active) {
-            console.log(payload)
           return (
             <div className="custom-tooltip">
                 <p className="name">{`Nome : ${payload[0].payload.name}`}</p>
@@ -27,9 +15,9 @@ function Grafico({data}){
       
         return null;
       };
-    return (        
+    return (      
         <ResponsiveContainer width="100%" height={600}>
-            <ScatterChart
+            <ScatterChart 
             margin={{
                 top: 20,
                 right: 300,
@@ -39,23 +27,21 @@ function Grafico({data}){
             }
             stroke="black"
             >
-                {/* <CartesianGrid stroke ={"rgba(0,0,0,1)"}/> */}
                 <XAxis 
                     type="number" dataKey="x" name="Valor Médio" unit="" label ={<Label  value="Valor Médio da Ação" position="bottom" offset={30}/>}
                     axisLine={{ stroke: 'black', strokeWidth: 2.5 }} tick={{ stroke: 'black' }} tickLine={{ stroke: 'black', strokeWidth: 2.5 }}
-                    tickSize={20}
+                    tickSize={12}
                 />
                 <YAxis 
-                type="number" dataKey="y" name="Média de ações" unit="" label ={<Label  value="Número médio  \nde ações em \n90 minutos" position="left"/>}
+                type="number" dataKey="y" name="Média de ações" unit="" label ={<Label value="Número médio de ações em 90 minutos" width={135} position="left" />}
                 axisLine={{ stroke: 'black', strokeWidth: 2.5 }} tick={{ stroke: 'black' }} tickLine={{ stroke: 'black', strokeWidth: 2.5}}
                 tickSize={30}
                 dx={27}
                 dy={-10}
                 />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} content = {<CustomTooltip />} />
-                <Legend verticalAlign='bottom' align='left' />
+                <Legend verticalAlign='bottom' align='center'/>
                 <Scatter name="Jogadores" data={data} fill="#90cf54"> 
-                    {/* <LabelList dataKey="x" /> */}
                 </Scatter>
             </ScatterChart>
         </ResponsiveContainer>
