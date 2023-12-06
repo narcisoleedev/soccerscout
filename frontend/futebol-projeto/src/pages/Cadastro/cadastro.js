edb5947edbca59ee60ccf586c374060a1f384213
GuilhermeC <guilhermecavalanti@gmail.com>
2023-12-06 18:51:45 -0300

    state adcionado no cadastro

--- frontend/futebol-projeto/src/pages/Cadastro/cadastro.js
+++ frontend/futebol-projeto/src/pages/Cadastro/cadastro.js
@@ -1,29 +1,45 @@
 import './cadastro.css'
+import {useState} from 'react'
 
 function Cadastro(){
+    const [formData, setFormData] = useState({
+        email: '',
+        nome: '',
+        senha: '',
+        senha2: '',
+      });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+      };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Aqui você pode adicionar lógica para enviar os dados para o servidor
+        console.log('Dados do formulário:', formData);
+      };
     return(
         <div className='Main'>
         <div className="side"></div>
         <div className='conteudo'>
             
             <div className='Formulario_Cadastro'>
-                <form action="/caminho/do-seu-script-de-processamento" method="post">
+                <form onSubmit={handleSubmit}>
                     <h1> Cadastre-se</h1>
                     {/*Campo de E-mail*/}
                     <label for="email">Endereço de email:</label>
-                    <input type="email" id="email" name="email" required>
+                    <input type="email" id="email" name="email" onChange={handleChange} value={formData.email} required>
                     </input>
                     {/*Campo de Nome*/}
                     <label for="nome">Nome de usuário:</label>
-                    <input type="text" id="nome" name="nome" required>
+                    <input type="text" id="nome" name="nome" onChange={handleChange} value={formData.nome} required>
                     </input>
                     {/*Campo de Senha*/}
                     <label for="senha">Senha:</label>
-                    <input type="password" id="senha" name="senha" required>
+                    <input type="password" id="senha" name="senha" onChange={handleChange} value={formData.senha} required>
                     </input>
                     {/*Campo de Senha 2*/}
                     <label for="senha2">Confirme Sua Senha:</label>
-                    <input type="password" id="senha2" name="senha2" required>
+                    <input type="password" id="senha2" name="senha2" onChange={handleChange} value={formData.senha2} required>
                     </input>
                     {/*Botão de envio*/}
                     <button type="submit">Criar Conta</button>
