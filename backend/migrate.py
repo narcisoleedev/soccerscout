160e1c01e6706df5ab8b24ea1a2b41a34f051502
41Guilherme41 <guilherme.nascimento@bcrcx.com>
2023-12-13 09:01:11 -0300

    feat: migrate default database

---
+++ backend/migrate.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+from services.database.connector import DATABASE_URI
+club = pd.read_csv("./csvs/club.csv", sep="|")
+jogadores = pd.read_csv("./csvs/jogadores.csv", sep="|")
+ligas = pd.read_csv("./csvs/ligas.csv", sep="|")
+
+club.to_sql("club", con=DATABASE_URI)
+jogadores.to_sql("player", con=DATABASE_URI)
+ligas.to_sql("league", con=DATABASE_URI)
