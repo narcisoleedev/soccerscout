071d7bc2da0d89febb9f1556f3098a0215a44244
rfgvieira <rfgvieira.01@gmail.com>
2023-12-16 03:32:44 -0300

    feat: profie page finished

--- frontend/futebol-projeto/src/components/PassInput/passinput.js
+++ frontend/futebol-projeto/src/components/PassInput/passinput.js
@@ -1,13 +1,22 @@
 import "./passinput.css"
-
-
+import React ,{ useState } from "react"
 import eye from "../../imagens/eye.webp"
 
 const PassInput = (props) => {
+
+    const [typeInput, setTypeInput] = useState("password")
+
+    const eyeClick = () => {
+        if(typeInput=== "password")
+            setTypeInput("text")
+        else
+            setTypeInput("password")
+    }
+
     return(
         <div className="input-pass">
-            <input type="password"  id={props.id} onChange={props.handleChange} value = {props.value} required />
-            <img src={eye} />
+            <input type={typeInput} autoComplete= {props.autocomplete} id={props.id} onChange={props.handleChange} value = {props.value} required />
+            <img src={eye} onClick = {eyeClick} />
         </div>
     )
 }
